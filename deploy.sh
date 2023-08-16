cdca1bb740a42e8ec8fe31dea25cec8a8e3b1fd4
hhhanzzz <ishhhan@icloud.com>
2023-08-16 17:47:47 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io 修改仓库地址
-git push -f git@github.com:hhhanzzz/hhhanzzzzzz.github.io.git master:gh-pages
+git push -f git@github.com:hhhanzzz/hhhanzzz.github.io.git master:gh-pages
 
 cd -
